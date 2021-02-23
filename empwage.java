6adf59fc9233986f1b7ac408644b5319abb1f231
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-23 21:42:53 +0530

    Added logic for checking if the employee is present or absent

--- empwage.java
+++ empwage.java
@@ -1,5 +1,17 @@
-public class empwagebuilder {
+public class empwage {
+	public void attendence(){
+		int ispresent=1;
+
+   	int empcheck = (int)Math.floor(Math.random() * 10)%2;
+
+   	if (empcheck == ispresent)
+      	System.out.println("Employee is present");
+   	else
+     		System.out.println("Employee is absent");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem using OOPS concept");
+		empwage ep=new empwage();
+		ep.attendence();
 	}
 }
