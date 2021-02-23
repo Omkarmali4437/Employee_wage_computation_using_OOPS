da13b47028f61663c5cb38a3b362c290d0958551
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-23 22:07:55 +0530

    Added logic for calculating employee daily wage

--- empwage.java
+++ empwage.java
@@ -1,17 +1,27 @@
 public class empwage {
-	public void attendence(){
-		int ispresent=1;
+	public void dailywage(){
+		int ispresent=1,empwage,emphour,dailywage=20;
 
    	int empcheck = (int)Math.floor(Math.random() * 10)%2;
 
    	if (empcheck == ispresent)
+		{
       	System.out.println("Employee is present");
-   	else
+			emphour=8;
+			empwage=dailywage*emphour;
+			System.out.println("Daily employee wage is: "+empwage);
+   	}
+		else
+		{
      		System.out.println("Employee is absent");
+			emphour=0;
+			empwage=dailywage*emphour;
+			System.out.println("Daily employee wage is: "+empwage);
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem using OOPS concept");
-		empwage ep=new empwage();
-		ep.attendence();
+		empwage ew=new empwage();
+		ew.dailywage();
 	}
 }
