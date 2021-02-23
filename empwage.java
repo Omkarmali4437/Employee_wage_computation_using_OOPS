495a82a42bd6e67922b4ca992195aea5946db6c3
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-23 22:19:00 +0530

    Added logic for a part time employee

--- empwage.java
+++ empwage.java
@@ -1,16 +1,29 @@
 public class empwage {
 	public void dailywage(){
-		int ispresent=1,empwage,emphour,dailywage=20;
+		int ispresent=1;
+		int isfulltime=1,isparttime=0,empwage,emphour,dailywage=20;
 
-   	int empcheck = (int)Math.floor(Math.random() * 10)%2;
+   	int attendence = (int)Math.floor(Math.random() * 10)%2;
 
-   	if (empcheck == ispresent)
+		if (attendence == ispresent)
 		{
-      	System.out.println("Employee is present");
-			emphour=8;
-			empwage=dailywage*emphour;
-			System.out.println("Daily employee wage is: "+empwage);
-   	}
+			System.out.println("Employee is present");
+			int empcheck = (int)Math.floor(Math.random() * 10)%2;
+			if(empcheck == isfulltime)
+			{
+				System.out.println("Employee is full time");
+				emphour=8;
+				empwage=dailywage*emphour;
+				System.out.println("Daily employee wage is: "+empwage);
+			}
+			else if(empcheck == isparttime)
+			{
+				System.out.println("Employee is part time");
+				emphour=4;
+				empwage=dailywage*emphour;
+				System.out.println("Daily employee wage is: "+empwage);
+			}
+		}
 		else
 		{
      		System.out.println("Employee is absent");
